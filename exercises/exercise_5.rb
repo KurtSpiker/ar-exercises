34102d4e05ece001c62953bb02a3d5ad7aca9f58
KurtSpiker <kurt.spiker@gmail.com>
2021-11-10 01:24:03 -0700

    exercises 1-7

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Company revenue: #{Store.sum(:annual_revenue)}"
+puts "Average company wide revenue: #{Store.average(:annual_revenue)}"
+puts "Number of stores that generate $1M or more in annual sales: #{Store.where("annual_revenue >= ?", 1000000).count}"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Steve", last_name: "S", hourly_rate: 61)
+@store1.employees.create(first_name: "Bob", last_name: "R", hourly_rate: 62)
+@store1.employees.create(first_name: "Jim", last_name: "J", hourly_rate: 63)
+@store1.employees.create(first_name: "Jake", last_name: "P", hourly_rate: 64)
+
+@store2.employees.create(first_name: "Mary", last_name: "M", hourly_rate: 65)
+@store2.employees.create(first_name: "Kate", last_name: "S", hourly_rate: 66)
+@store2.employees.create(first_name: "Erin", last_name: "W", hourly_rate: 67)
+@store2.employees.create(first_name: "Emma", last_name: "A", hourly_rate: 68)
